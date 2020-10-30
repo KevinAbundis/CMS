623081eb7f4b30958520ffea2ff60e395055c491
KeviinAbundiiz <abundiiz_keviin@outlook.es>
2020-10-30 00:04:16 -0600

    list of categories and form of search in home ok

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,35 @@
 @extends('master')
 
 @section('title', 'Inicio')
+
+@section('content')
+	<div class="home_action_bar shadow">
+		<div class="row">
+			<div class="col-md-3">
+				<div class="categories">
+					<a href="#" class="link"><i class="fas fa-stream"></i> Categorías</a>
+					<ul class="shadow">
+						@foreach($categories as $category)
+							<li>
+								<a href="{{ url('/store/category/'.$category->id.'/'.$category->slug) }}">
+									<img src="{{ url('/uploads/'.$category->file_path.'/'.$category->icono) }}" alt="">
+									{{ $category->name }}
+								</a>
+							</li>
+						@endforeach
+					</ul>
+				</div>
+			</div>
+
+			<div class="col-md-9">
+				{!! Form::open(['url' => '/search']) !!}
+				<div class="input-group">
+					 <i class="fas fa-search"></i>
+					 {!! Form::text('search_query', null, ['class' => 'form-control', 'placeholder' => 'Ingrese su búsqueda', 'required']) !!}
+					 <button class="btn" type="submit" id="button-addon2">Buscar</button>
+				</div>
+				{!! Form::close() !!}
+			</div>
+		</div>
+	</div>
+@endsection
